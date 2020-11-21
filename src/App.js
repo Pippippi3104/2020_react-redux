810ee8234ae999f24d3766133fefd9c3970f8bd9
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-21 21:21:12 +0900

    # finish editing lesson15

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React, {Componet} from 'react';
+import React, {Componet} from "react";
 
-function App() {
-/*   return (
-    <h1>
-      Hello World.
-    </h1>
-  ); */
+/* class App extends Componet {
+  render() { */
+  /*   return (
+      <h1>
+        Hello World.
+      </h1>
+    ); */
 
-  /* const greeting = "Hi, Tom !";
-  const dom = <h1 classname="foo">{greeting}</h1>
-  return dom; */
+    /* const greeting = "Hi, Tom !";
+    const dom = <h1 classname="foo">{greeting}</h1>
+    return dom; */
 
-/*   return (
-    <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => {console.log("I'm clicking now !")}} />
-    </div>
-  ); */
+  /*   return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => {console.log("I'm clicking now !")}} />
+      </div>
+    ); */
 
-  //babel repl -> JSXをJSに変換後のコードが見れる
+    //babel repl -> JSXをJSに変換後のコードが見れる
 
+/*     return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => {console.log("I'm clicking now !")}} />
+      </React.Fragment>
+    );
+  }
+} */
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => {console.log("I'm clicking now !")}} />
-    </React.Fragment>
-  );
+  <div>
+    Hi!
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  ) 
+}
+
+const Cat = () => {
+  return (
+    <div>
+      Meow!
+    </div>
+  )
 }
 
 export default App;
