0684cd1a005cc979e918f362f4c045a58542653d
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-25 01:18:25 +0900

    # note the progress - 01

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,9 +1,12 @@
+import _ from "lodash";
 import { READ_EVENTS } from "../actions";
 
 // eslint-disable-next-line
-export default (state = {}, actions) => {
-    switch (actions.type) {
+export default (state = {}, action) => {
+    switch (action.type) {
         case READ_EVENTS:
+            console.log(action.response.data);
+            console.log(_.mapKeys(action.response.data, "id"));
             return state
         default:
             return state
