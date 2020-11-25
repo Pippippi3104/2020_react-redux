f73b4dd6f43237755ce2aec30f5897cf1f217ff4
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-25 23:24:15 +0900

    # note the progress - 02

--- src/component/events_new.js
+++ src/component/events_new.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
 //import { postEvent } from "../actions";
 
 class EventsNew extends Component {
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field;
+
+    return (<div></div>);
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
+      <form>
+        <div>
+          <Field label="Title" name="title" type="text" component={this.renderField} />
+        </div>
+        <div>
+          <Field label="Body" name="body" type="text" component={this.renderField} />
+        </div>
+      </form>
     )
   }
 }
